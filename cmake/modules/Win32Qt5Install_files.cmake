2bfc29b30e2587637f8d1c439f56c4916fec6c75
Vitaly Tonkacheyev <thetvg@gmail.com>
2017-03-10 11:49:58 +0200

    Cmake scripts updated

--- cmake/modules/Win32Qt5Install_files.cmake
+++ cmake/modules/Win32Qt5Install_files.cmake
@@ -1,134 +1,162 @@
-cmake_minimum_required( VERSION 2.8.12 )
-if( CMAKE_VERSION VERSION_GREATER 3.0.0 )
-	cmake_policy(SET CMP0028 OLD)
+cmake_minimum_required( VERSION 3.0.2 )
+if(CMAKE_BUILD_TYPE STREQUAL "Debug" AND WIN32)
+	set(D "d")
+	add_definitions(-DALLOW_QT_PLUGINS_DIR)
 endif()
 
-if( ${USE_QT5} )
-	#install taglib
-	find_package( TagLib )
-	if( TAGLIB_FOUND )
-		find_file( _libtag libtag.dll PATHES ${TAGLIB_ROOT}/bin ${TAGLIB_INCLUDE_DIR}/../bin )
-		if( NOT "${_libtag}" STREQUAL "_libtag-NOTFOUND" )
-			INSTALL( FILES ${_libtag} DESTINATION ${CMAKE_INSTALL_PREFIX} )
-		endif()
-	endif()
-	#install libcue
-	find_package( LibCue )
-	if( LIBCUE_FOUND )
-		find_file( _libcue libcue.dll PATHES ${LIBCUE_ROOT}/bin ${LIBCUE_INCLUDE_DIR}/../bin )
-		if( NOT "${_libcue}" STREQUAL "_libcue-NOTFOUND" )
-			INSTALL( FILES ${_libcue} DESTINATION ${CMAKE_INSTALL_PREFIX} )
-		endif()
-	endif()
-	#install OpenSSL
-	set( OPENSSL_ROOT_DIR "c:\\build\\psibuild\\psideps\\Qt5\\openssl" CACHE STRING "OpenSSL root path" )
-	set( ZLIB_ROOT "c:\\build\\psibuild\\psideps\\Qt5\\zlib" CACHE STRING "ZLIB root path" )
-	find_package( OpenSSL QUIET )
-	if( OPENSSL_FOUND )
-		find_file( _libssl ssleay32.dll PATHES ${OPENSSL_ROOT_DIR}/bin ${OPENSSL_ROOT_DIR}/lib )
-		if( NOT "${_libssl}" STREQUAL "_libssl-NOTFOUND" )
-			INSTALL( FILES ${_libssl} DESTINATION ${CMAKE_INSTALL_PREFIX} )
-		endif()
-		find_file( _libeay libeay32.dll PATHES ${OPENSSL_ROOT_DIR}/bin ${OPENSSL_ROOT_DIR}/lib )
-		if( NOT "${_libeay}" STREQUAL "_libeay-NOTFOUND" )
-			INSTALL( FILES ${_libeay} DESTINATION ${CMAKE_INSTALL_PREFIX} )
-		endif()
-	endif()
-	#install ZLIB
-	find_package( ZLIB QUIET )
-	if( ZLIB_FOUND )
-		find_file( _libz zlib1.dll PATHES ${ZLIB_ROOT}/bin ${ZLIB_ROOT}/lib )
-		if( NOT "${_libz}" STREQUAL "_libz-NOTFOUND" )
-			INSTALL( FILES ${_libz} DESTINATION ${CMAKE_INSTALL_PREFIX} )
-		endif()
-	endif()
-	#install Qt5 libs and plugins
-	set( qt5_components
-		Core
-		Gui
-		Network
-		Multimedia
-		Xml
-		Widgets
-		Concurrent
-		WinExtras
-	)
-	find_package( Qt5 COMPONENTS ${qt5_components} REQUIRED )
-	foreach( liba ${qt5_components} )
-		get_target_property( _libloc Qt5::${liba} LOCATION )
-		if( _libloc )
-			INSTALL( FILES ${_libloc} DESTINATION ${CMAKE_INSTALL_PREFIX} )
-		endif()
-	endforeach()
-	set( INST_PLATFORM_PLUGS
-		QMinimalIntegrationPlugin
-		QWindowsIntegrationPlugin
-	)
-	foreach( plug ${INST_PLATFORM_PLUGS} )
-		get_target_property( _libloc Qt5::${plug} LOCATION )
-		if( _libloc )
-			INSTALL( FILES ${_libloc} DESTINATION ${CMAKE_INSTALL_PREFIX}/platforms )
-		endif()
-	endforeach()
-	set( INST_NETWORK_PLUGS
-		QNativeWifiEnginePlugin
-		QGenericEnginePlugin
-	)
-	foreach( plug ${INST_NETWORK_PLUGS} )
-		get_target_property( _libloc Qt5::${plug} LOCATION )
-		if( _libloc )
-			INSTALL( FILES ${_libloc} DESTINATION ${CMAKE_INSTALL_PREFIX}/bearer )
-		endif()
-	endforeach()
-	get_target_property( _audio Qt5::QWindowsAudioPlugin LOCATION )
-	if( _audio )
-		INSTALL( FILES ${_audio} DESTINATION ${CMAKE_INSTALL_PREFIX}/audio )
-	endif()
-	get_target_property( _plformats Qt5::QM3uPlaylistPlugin LOCATION )
-	if( _plformats )
-		INSTALL( FILES ${_plformats} DESTINATION ${CMAKE_INSTALL_PREFIX}/playlistformats )
-	endif()
-	set( INST_MEDIA_PLUGS
-		AudioCaptureServicePlugin
-		DSServicePlugin
-	)
-	foreach( plug ${INST_MEDIA_PLUGS} )
-		get_target_property( _libloc Qt5::${plug} LOCATION )
-		if( _libloc )
-			INSTALL( FILES ${_libloc} DESTINATION ${CMAKE_INSTALL_PREFIX}/mediaservice )
-		endif()
-	endforeach()
-	#install mingw and other needed libs
-	set( ICU_LIBS_PREFIXES
-		icudt5
-		icuin5
-		icuuc5
-	)
-	set( ICU_LIBS "" )
-	foreach( icu_prefix ${ICU_LIBS_PREFIXES} )
-		foreach( icu_counter RANGE 9 )
-			find_file( ${icu_prefix}${icu_counter} "${icu_prefix}${icu_counter}.dll" )
-			if( NOT "${${icu_prefix}${icu_counter}}" STREQUAL "${icu_prefix}${icu_counter}-NOTFOUND" )
-				set( ICU_LIBS ${ICU_LIBS} ${${icu_prefix}${icu_counter}} )
-			endif()
-		endforeach()
-	endforeach()
-	if( ICU_LIBS )
-		INSTALL( FILES ${ICU_LIBS} DESTINATION ${CMAKE_INSTALL_PREFIX} )
-	endif()
+option(USE_MXE "Use MXE toolkit" OFF)
 
-	set( FILE_LIST
-		libgcc_s_sjlj-1.dll
-		libgcc_s_dw2-1.dll
-		libstdc++-6.dll
-		libwinpthread-1.dll
+string(REGEX REPLACE "([^ ]+)[/\\].*" "\\1" QT_BIN_DIR_TMP "${QT_MOC_EXECUTABLE}")
+string(REGEX REPLACE "\\\\" "/" QT_BIN_DIR "${QT_BIN_DIR_TMP}")
+unset(QT_BIN_DIR_TMP)
+get_filename_component(QT_DIR ${QT_BIN_DIR} DIRECTORY)
+set(QT_PLUGINS_DIR ${QT_DIR}/plugins)
+
+function(copy SOURCE DEST TARGET)
+	if(EXISTS ${SOURCE})
+		set(OUT_TARGET_FILE "${CMAKE_BINARY_DIR}/${TARGET}.cmake")
+
+		string(REGEX REPLACE "\\\\+" "/" DEST "${DEST}")
+		string(REGEX REPLACE "\\\\+" "/" SOURCE "${SOURCE}")
+
+		if(NOT TARGET ${TARGET})
+			file(REMOVE "${OUT_TARGET_FILE}")
+			add_custom_target(
+				${TARGET}
+				COMMAND
+				${CMAKE_COMMAND} -P "${OUT_TARGET_FILE}"
+			)
+		endif()
+
+		if(IS_DIRECTORY ${SOURCE})
+			# copy directory
+			file(GLOB_RECURSE FILES "${SOURCE}/*")
+			get_filename_component(SOURCE_DIR_NAME ${SOURCE} NAME)
+
+			foreach(FILE ${FILES})
+				file(RELATIVE_PATH REL_PATH ${SOURCE} ${FILE})
+				set(REL_PATH "${SOURCE_DIR_NAME}/${REL_PATH}")
+				get_filename_component(REL_PATH ${REL_PATH} DIRECTORY)
+				set(DESTIN "${DEST}/${REL_PATH}")
+
+				string(REGEX REPLACE "/+" "/" DESTIN ${DESTIN})
+				string(REGEX REPLACE "/+" "/" FILE ${FILE})
+
+				file(APPEND
+					"${OUT_TARGET_FILE}"
+					"file(INSTALL \"${FILE}\" DESTINATION \"${DESTIN}\" USE_SOURCE_PERMISSIONS)\n")
+			endforeach()
+		else()
+			string(REPLACE "//" "/" DEST ${DEST})
+			if(DEST MATCHES "/$")
+				set(DIR "${DEST}")
+				string(REGEX REPLACE "^(.+)/$" "\\1" DIR ${DIR})
+			else()
+				# need to copy and rename
+				get_filename_component(DIR ${DEST} DIRECTORY)
+				get_filename_component(FILENAME ${DEST} NAME)
+				get_filename_component(SOURCE_FILENAME ${SOURCE} NAME)
+			endif()
+			file(APPEND
+				"${OUT_TARGET_FILE}"
+				"file(INSTALL \"${SOURCE}\" DESTINATION \"${DIR}\" USE_SOURCE_PERMISSIONS)\n")
+			if(DEFINED FILENAME)
+				file(APPEND
+					"${OUT_TARGET_FILE}"
+					"file(RENAME \"${DIR}/${SOURCE_FILENAME}\" \"${DIR}/${FILENAME}\")\n")
+			endif()
+		endif()
+	endif()
+endfunction()
+
+
+#install Qt5 libs and plugins
+set(LIBS_LIST "")
+copy("${QT_BIN_DIR}/Qt5Core${D}.dll" "${EXECUTABLE_OUTPUT_PATH}/" prepare-bin)
+copy("${QT_BIN_DIR}/Qt5Gui${D}.dll" "${EXECUTABLE_OUTPUT_PATH}/" prepare-bin)
+copy("${QT_BIN_DIR}/Qt5Widgets${D}.dll" "${EXECUTABLE_OUTPUT_PATH}/" prepare-bin)
+copy("${QT_BIN_DIR}/Qt5Network${D}.dll" "${EXECUTABLE_OUTPUT_PATH}/" prepare-bin)
+copy("${QT_BIN_DIR}/Qt5Xml${D}.dll" "${EXECUTABLE_OUTPUT_PATH}/" prepare-bin)
+copy("${QT_BIN_DIR}/Qt5XmlPatterns${D}.dll" "${EXECUTABLE_OUTPUT_PATH}/" prepare-bin)
+copy("${QT_BIN_DIR}/Qt5Multimedia${D}.dll" "${EXECUTABLE_OUTPUT_PATH}/" prepare-bin)
+copy("${QT_BIN_DIR}/Qt5MultimediaWidgets${D}.dll" "${EXECUTABLE_OUTPUT_PATH}/" prepare-bin)
+copy("${QT_BIN_DIR}/Qt5Concurrent${D}.dll" "${EXECUTABLE_OUTPUT_PATH}/" prepare-bin)
+copy("${QT_BIN_DIR}/Qt5OpenGL${D}.dll" "${EXECUTABLE_OUTPUT_PATH}/" prepare-bin)
+copy("${QT_BIN_DIR}/Qt5WinExtras${D}.dll" "${EXECUTABLE_OUTPUT_PATH}/" prepare-bin)
+copy("${QT_PLUGINS_DIR}/audio/qtaudio_windows${D}.dll" "${EXECUTABLE_OUTPUT_PATH}/audio/" prepare-bin)
+copy("${QT_PLUGINS_DIR}/platforms/qminimal${D}.dll" "${EXECUTABLE_OUTPUT_PATH}/platforms/" prepare-bin)
+copy("${QT_PLUGINS_DIR}/platforms/qoffscreen${D}.dll" "${EXECUTABLE_OUTPUT_PATH}/platforms/" prepare-bin)
+copy("${QT_PLUGINS_DIR}/platforms/qwindows${D}.dll" "${EXECUTABLE_OUTPUT_PATH}/platforms/" prepare-bin)
+copy("${QT_PLUGINS_DIR}/bearer/qgenericbearer${D}.dll" "${EXECUTABLE_OUTPUT_PATH}/bearer/" prepare-bin)
+copy("${QT_PLUGINS_DIR}/bearer/qnativewifibearer${D}.dll" "${EXECUTABLE_OUTPUT_PATH}/bearer/" prepare-bin)
+copy("${QT_PLUGINS_DIR}/playlistformats/qtmultimedia_m3u${D}.dll" "${EXECUTABLE_OUTPUT_PATH}/playlistformats/" prepare-bin)
+copy("${QT_PLUGINS_DIR}/mediaservice/dsengine${D}.dll" "${EXECUTABLE_OUTPUT_PATH}/mediaservice/" prepare-bin)
+copy("${QT_PLUGINS_DIR}/mediaservice/qtmedia_audioengine${D}.dll" "${EXECUTABLE_OUTPUT_PATH}/mediaservice/" prepare-bin)
+
+#install mingw and other needed libs
+set( FILE_LIST
+	libgcc_s_sjlj-1.dll
+	libgcc_s_dw2-1.dll
+	libgcc_s_seh-1.dll
+	libstdc++-6.dll
+	libwinpthread-1.dll
+)
+set( ICU_LIBS_PREFIXES
+	icudt5
+	icuin5
+	icuuc5
+)
+
+foreach( icu_prefix ${ICU_LIBS_PREFIXES} )
+	foreach( icu_counter RANGE 9 )
+		find_file( ${icu_prefix}${icu_counter} "${icu_prefix}${icu_counter}.dll" )
+		if( EXISTS "${${icu_prefix}${icu_counter}}" )
+			copy("${${icu_prefix}${icu_counter}}" "${EXECUTABLE_OUTPUT_PATH}/" prepare-bin)
+		endif()
+	endforeach()
+endforeach()
+
+if(USE_MXE)
+	list(APPEND FILE_LIST
+		libbz2.dll
+		libfreetype-6.dll
+		libglib-2.0-0.dll
+		libharfbuzz-0.dll
+		libharfbuzz-icu-0.dll
+		libiconv-2.dll
+		libintl-8.dll
+		libjasper-1.dll
+		libjpeg-9.dll
+		liblcms2-2.dll
+		liblzma-5.dll
+		liblzo2-2.dll
+		libmng-2.dll
+		libpcre16-0.dll
+		libpcre-1.dll
+		libpng16-16.dll
+		libtiff-5.dll
 	)
-	set( inc 1 )
-	foreach( liba ${FILE_LIST} )
-		find_file( ${liba}${inc} ${liba} )
-		if( NOT "${${liba}${inc}}" STREQUAL "${liba}${inc}-NOTFOUND" )
-			INSTALL( FILES ${${liba}${inc}} DESTINATION ${CMAKE_INSTALL_PREFIX} )
+endif()
+
+list(APPEND FILE_LIST
+	zlib1.dll
+	libeay32.dll
+	ssleay32.dll
+	libtag.dll
+	libcue.dll
+)
+
+macro(find_lib)
+	set(LIBLIST ${ARGN})
+		set( inc 1 )
+		foreach(_liba ${LIBLIST})
+			find_file( ${_liba}${inc} ${_liba} )
+			if( NOT "${${_liba}${inc}}" STREQUAL "${_liba}${inc}-NOTFOUND" )
+				copy("${${_liba}${inc}}" "${EXECUTABLE_OUTPUT_PATH}/" prepare-bin)
 		endif()
 		math( EXPR inc ${inc}+1)
 	endforeach()
-endif( ${USE_QT5} ) 
+endmacro()
+
+find_lib(${FILE_LIST})
+
